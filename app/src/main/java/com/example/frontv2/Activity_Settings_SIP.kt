e8cf518c9a49a92cddfb63c9b4f2195049007cd2
Vasiliy Yurchik <134363577+VYyurchik@users.noreply.github.com>
2023-06-18 17:59:12 +0700

    Front v1.0
    Я добавил фронт!

---
+++ app/src/main/java/com/example/frontv2/Activity_Settings_SIP.kt
@@ -0,0 +1,29 @@
+package com.example.frontv2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class Activity_Settings_SIP : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings_sip)
+    }
+    fun onClickGoAunth(view : View){
+        val intent = Intent(this, ActivityAunth::class.java)
+        startActivity(intent)
+    }
+    fun onClickGoCalls(view : View){
+        val intent = Intent(this, ActivityCalls::class.java)
+        startActivity(intent)
+    }
+    fun onClickGoConviviis(view : View){
+        val intent = Intent(this, ActivityConviviis::class.java)
+        startActivity(intent)
+    }
+    fun onClickGoEmail(view : View){
+        val intent = Intent(this, ActivityEmail::class.java)
+        startActivity(intent)
+    }
+}
